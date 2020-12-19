8a5329710d040dcf2fe71836dd700911e4ef91fb
azriakmalk <azriakmal2222@gmail.com>
2020-12-19 11:32:03 +0700

    cors

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors())
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin",'*')
     res.header("Access-Control-Allow-Origin",'*')
-    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELET")
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE")
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-auth-token")
     next();
 })
